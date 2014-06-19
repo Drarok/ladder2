9cec52e362dd408ad2bbb195468f1f343595fc48
Mat Gadd <mat@zerifas.com>
2014-06-19 14:31:33 +0100

    Split the run process into a bootstrap and simple file to give users and end point for customisation.

---
+++ bootstrap.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$container = new Pimple();
+
+$container['config'] = function ($container) {
+    return array(
+        'dsn'      => 'mysql:host=localhost;dbname=test;charset=utf8;',
+        'username' => 'root',
+        'password' => '',
+    );
+};
+
+$container['db'] = function ($container) {
+    $config = $container['config'];
+    return new PDO(
+        $config['dsn'],
+        $config['username'],
+        $config['password'],
+        [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_STRINGIFY_FETCHES  => false,
+        ]
+    );
+};
+
+$container['migrationManager'] = function ($container) {
+    return new Ladder\MigrationManager($container);
+};
+
+$container['app'] = function ($container) {
+    $app = new Symfony\Component\Console\Application('Ladder2', Ladder\Version::getVersion());
+
+    $app->addCommands([
+        new Ladder\Command\LadderCommand($container),
+    ]);
+
+    return $app;
+};
+
+return $container;
--- ladder.php
+++ ladder.php
@@ -1,12 +1,6 @@
 #!/usr/bin/env php
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+$container = require_once __DIR__ . '/bootstrap.php';
 
-$app = new Symfony\Component\Console\Application('Ladder2', Ladder\Version::getVersion());
-
-$app->addCommands([
-    new Ladder\Command\LadderCommand(),
-]);
-
-$app->run();
+$container['app']->run();
