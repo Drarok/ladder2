2c1c92c17d752db58e8465f3410e77e09543bb04
Mat Gadd <mat@zerifas.com>
2014-06-19 16:46:24 +0100

    Add config loading to “binary”.

--- ladder.php
+++ ladder.php
@@ -3,4 +3,10 @@
 
 $container = require_once __DIR__ . '/bootstrap.php';
 
+$configPathname = Ladder\Path::join(getcwd(), 'ladder.json');
+
+if (is_file($configPathname)) {
+    $container['config'] = json_decode(file_get_contents($configPathname), true);
+}
+
 $container['app']->run();
