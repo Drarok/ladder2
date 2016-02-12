4c01ec77b9ec1e6372bf3bff9f3c05e3aaffe234
Mat Gadd <mat@zerifas.com>
2016-02-12 15:31:57 +0000

    Add a typehint

--- src/Ladder/Arr.php
+++ src/Ladder/Arr.php
@@ -4,7 +4,7 @@ namespace Ladder;
 
 abstract class Arr
 {
-    public static function get($arr, $key, $default = null)
+    public static function get(array $arr, $key, $default = null)
     {
         return array_key_exists($key, $arr) ? $arr[$key] : $default;
     }
